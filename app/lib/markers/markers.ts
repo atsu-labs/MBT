5c3733bb66434e3692da08dc59a5bb8cd1ecce6d
A.K <atsu.enterprise@gmail.com>
2026-06-23 17:08:20 +0900

    feat: アミノバイタルのエイドマーカーを削除 (#33)
    * feat: markers.tsからアミノバイタルのエイドマーカーを削除
    
    * style: 定数定義の波括弧の位置を統一
    
    ---------
    
    Co-authored-by: Atsu <atsu.enterprize@gmail.com>

--- app/lib/markers/markers.ts
+++ app/lib/markers/markers.ts
@@ -1,333 +1,331 @@
-export const firstAid ={
-    "type": "FeatureCollection",
-    "features": [
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第１救護所",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.772275,
-            41.775792
-          ]
-        }
+export const firstAid = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第１救護所",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
       },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第２救護所",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.739965,
-            41.769923
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第３救護所",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.720739,
-            41.761193
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第４救護所",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.726935,
-            41.786493
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第５救護所",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.710922,
-            41.769063
-          ]
-        }
-      },
-    ]
-  };
-  export const gate =
-  {
-    "type": "FeatureCollection",
-    "features": [
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第２関門",
-          "comment":"閉鎖時刻10:19",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.790895,
-            41.7749
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第３関門",
-          "comment":"閉鎖時刻11:00",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.82756,
-            41.765942
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第４関門",
-          "comment":"閉鎖時刻11:40",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.772446,
-            41.776
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第５関門",
-          "comment":"閉鎖時刻12:24",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.720503,
-            41.760893
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第６関門",
-          "comment":"閉鎖時刻13:08",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.740512,
-            41.777065
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第７関門",
-          "comment":"閉鎖時刻13:44",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.720084,
-            41.769191
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第８関門",
-          "comment":"閉鎖時刻14:17",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.719178,
-            41.768663
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第９関門",
-          "comment":"閉鎖時刻14:40",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.726988,
-            41.782489
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "第１関門",
-          "comment":"閉鎖時刻9:54",
-          "_markerType": "Icon",
-          "_iconSize": [
-            20,
-            20
-          ],
-          "_iconAnchor": [
-            10,
-            10
-          ]
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            140.765955,
-            41.781489
-          ]
-        }
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.772275,
+          41.775792
+        ]
       }
-    ]
-  };
-export const aid =
-{
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第２救護所",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.739965,
+          41.769923
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第３救護所",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.720739,
+          41.761193
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第４救護所",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.726935,
+          41.786493
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第５救護所",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.710922,
+          41.769063
+        ]
+      }
+    },
+  ]
+};
+export const gate = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第２関門",
+        "comment": "閉鎖時刻10:19",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.790895,
+          41.7749
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第３関門",
+        "comment": "閉鎖時刻11:00",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.82756,
+          41.765942
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第４関門",
+        "comment": "閉鎖時刻11:40",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.772446,
+          41.776
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第５関門",
+        "comment": "閉鎖時刻12:24",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.720503,
+          41.760893
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第６関門",
+        "comment": "閉鎖時刻13:08",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.740512,
+          41.777065
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第７関門",
+        "comment": "閉鎖時刻13:44",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.720084,
+          41.769191
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第８関門",
+        "comment": "閉鎖時刻14:17",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.719178,
+          41.768663
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第９関門",
+        "comment": "閉鎖時刻14:40",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.726988,
+          41.782489
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "第１関門",
+        "comment": "閉鎖時刻9:54",
+        "_markerType": "Icon",
+        "_iconSize": [
+          20,
+          20
+        ],
+        "_iconAnchor": [
+          10,
+          10
+        ]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          140.765955,
+          41.781489
+        ]
+      }
+    }
+  ]
+};
+export const aid = {
   "type": "FeatureCollection",
   "features": [
     {
@@ -637,34 +635,11 @@ export const aid =
           41.786385
         ]
       }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "name": "アミノバイタル",
-        "_markerType": "Icon",
-        "_iconSize": [
-          20,
-          20
-        ],
-        "_iconAnchor": [
-          10,
-          10
-        ]
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          140.735051,
-          41.779825
-        ]
-      }
     }
   ]
 };
 
-export const aed =
-{
+export const aed = {
   "type": "FeatureCollection",
   "features": [
     {
